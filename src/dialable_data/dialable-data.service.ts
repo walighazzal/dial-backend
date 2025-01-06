2fa2d8a9f0247d6ef63e4a951fe056a3f19fd764
SahilPasha577 <sahilalipasha577@gmail.com>
2025-01-06 18:32:59 +0500

    added logic to upload multiple excel files

--- src/dialable_data/dialable-data.service.ts
+++ src/dialable_data/dialable-data.service.ts
@@ -21,16 +21,76 @@ export class DialableDataService {
     return { id: vendor.vendorId, name: vendorName };
   }
 
-  async processFile(
-    file: Express.Multer.File,
+  // async processFile(
+  //   file: Express.Multer.File,
+  //   vendorId: string,
+  //   createdBy: string,
+  // ) {
+  //   const parsedData = parseFile(file);
+  //   const uniqueNumbers = new Set<string>();
+  //   const duplicates = new Set<string>();
+
+  //   parsedData.forEach((row) => {      
+  //     if (uniqueNumbers.has(row.phone_number)) {
+  //       duplicates.add(row.phone_number);
+  //     } else {
+  //       uniqueNumbers.add(row.phone_number);
+  //     }
+  //   });
+
+  //   const stateDetails = Array.from(uniqueNumbers).map((phone_number) => ({
+  //     phone_number,
+  //     ...calculateStateDetails(phone_number),
+  //   }
+  // ));
+
+  // console.log(stateDetails);
+
+  // stateDetails.map((data)=> {
+  //   this.dataSource.manager.save(DialableData, {
+  //     createdBy: createdBy,
+  //     stateName: data.stateName,
+  //     stateCode: data.stateCode,
+  //     areaCode: data.areaCode,
+  //     number: data.phone_number
+  // })  
+  // })
+
+    
+
+  //   // Save to dialable-data entity 
+    
+
+  //   // Return result
+  //   return {
+  //     uploadId: 'generated-upload-id',
+  //     // newRecords: stateDetails.length - duplicates.size,
+  //     newRecords: stateDetails.length,
+  //     duplicateRecords: duplicates.size,
+      
+  //   };
+  // }
+
+
+
+  
+
+  async createVendor(createVendorDto: CreateVendorDto) {
+    return this.dataSource.manager.save(Vendor, createVendorDto);
+    // return { vendorId: 'new-vendor-id', ...createVendorDto };
+  }
+
+  async processFiles(
+    files: Express.Multer.File[],
     vendorId: string,
     createdBy: string,
   ) {
-    const parsedData = parseFile(file);
+    const allParsedData = files.flatMap((file) => parseFile(file));
+
     const uniqueNumbers = new Set<string>();
     const duplicates = new Set<string>();
 
-    parsedData.forEach((row) => {      
+    allParsedData.forEach((row) => {
       if (uniqueNumbers.has(row.phone_number)) {
         duplicates.add(row.phone_number);
       } else {
@@ -41,27 +101,23 @@ export class DialableDataService {
     const stateDetails = Array.from(uniqueNumbers).map((phone_number) => ({
       phone_number,
       ...calculateStateDetails(phone_number),
-    }
-  ));
-
-  console.log(stateDetails);
-
-  stateDetails.map((data)=> {
-    this.dataSource.manager.save(DialableData, {
-      createdBy: createdBy,
-      stateName: data.stateName,
-      stateCode: data.stateCode,
-      areaCode: data.areaCode,
-      number: data.phone_number
-  })  
-  })
-
+    }));
     
 
-    // Save to dialable-data entity 
-    
+    await Promise.all(
+      stateDetails.map((data) =>
+        this.dataSource.manager.save(DialableData, {
+          createdBy,
+          stateName: data.stateName,
+          stateCode: data.stateCode,
+          areaCode: data.areaCode,
+          number: data.phone_number,
+        }),
+      ),
+    );
 
-    // Return result
+
+  // Return result
     return {
       uploadId: 'generated-upload-id',
       // newRecords: stateDetails.length - duplicates.size,
@@ -69,10 +125,9 @@ export class DialableDataService {
       duplicateRecords: duplicates.size,
       
     };
-  }
+  
 
-  async createVendor(createVendorDto: CreateVendorDto) {
-    return this.dataSource.manager.save(Vendor, createVendorDto);
-    // return { vendorId: 'new-vendor-id', ...createVendorDto };
-  }
+
+}
+
 }
